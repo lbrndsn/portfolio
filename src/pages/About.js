1f6fbaf6543de3d59ec3c75dacea44a177c5c0ae
milo <milotolboom@gmail.com>
2020-10-16 22:15:10 +0200

    first paragraph and page alignment of about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,17 @@ import React from "react";
 
 const About = () => {
     return (
-        <div className="fixed">
-            about
+        <div className="fixed page">
+            <div className="page-inner">
+                <h1>about me</h1>
+                <p>I’m Loïs. I'm an interaction designer living in Arnhem. In my spare time I have a couple of hobbies
+                    which
+                    include playing the guitar, cooking and fitness/weightlifting. I enjoy programming and I'm working
+                    on
+                    getting more skilled in that area. I aspire to find a job in which I can combine interaction design
+                    and
+                    programming. I also love dogs. 🐕</p>
+            </div>
         </div>
     )
 };
