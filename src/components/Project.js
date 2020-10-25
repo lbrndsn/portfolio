180abfbb272c2724f931780e5bb390ad77c418c3
milo <milotolboom@gmail.com>
2020-10-25 20:04:09 +0100

    expand/collapse button

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,24 +2,43 @@ import React, {useEffect, useState} from "react";
 import {observer} from "mobx-react-lite";
 
 const Project = observer(({ title, shortDescription, tags, children }) => {
-    // const [actualProgress, setProgress] = useState(0);
+    const [isOpen, setIsOpen] = useState(false);
 
     let slicePosition = 2;
     if (tags.length === 3) {
         slicePosition = 3;
     }
-    const firstTagItems = tags.slice(0, slicePosition).map(tag => <div className="tag">{tag}</div>);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    const firstTagItems = tags.slice(0, slicePosition).map(tag => <div className="tag" onClick={toggle}>{tag}</div>);
     const moreTagItems = tags.slice(slicePosition - 1, tags.length - 1).map(tag => <div className="tag tag-more">{tag}</div>);
+    const allTags = tags.map(tag => <div className="tag tag-more">{tag}</div>);
+
+    const notOpenTagView = (
+        <>
+            {firstTagItems}
+            {moreTagItems.length > 0 && <div className="tag tag-more" onClick={toggle}>AND {moreTagItems.length} MORE</div>}
+        </>
+    );
 
     return (
         <div className="project-container">
             <h2>{title}</h2>
-            <p>{shortDescription}</p>
-            <div className="tag-container">
-                {firstTagItems}
-                {moreTagItems.length > 0 && <div className="tag tag-more">AND {moreTagItems.length} MORE</div>}
+
+            <div className="project-open-button-container">
+                <div className="project-open-button" onClick={toggle}>
+                    {isOpen ? "Collapse" : "Expand"}
+                </div>
             </div>
-            {children}
+
+            <p>{shortDescription}</p>
+
+            <div className="tag-container">
+                {isOpen ? allTags : notOpenTagView}
+            </div>
+
+            {isOpen && children}
         </div>
     )
 });
