78f2815e3159e5603723bf9306bcbce1ee6aeed8
Loïs Berndsen <me@loisberndsen.nl>
2020-11-01 22:36:03 +0100

    fixed some margins

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,14 +15,9 @@ const About = observer(({ id }) => {
         <div className="fixed page" ref={ref}>
             <div className="page-inner">
                 <h1>about me</h1>
-                <p>I’m Loïs. I'm an interaction designer living in Arnhem. In my spare time I have a couple of hobbies
-                    which
-                    include playing the guitar, cooking and fitness / weightlifting. I enjoy programming and I'm working
-                    on
-                    getting more skilled in that area. I aspire to find a job in which I can combine interaction design
-                    and
-                    programming. I also love dogs. 🐕</p>
-
+                <p>
+                    I’m Loïs. I'm an interaction designer living in Arnhem. In my spare time I have a couple of hobbies which include playing the guitar, fitness and cooking. I enjoy programming and I'm working on getting more skilled in that area. I aspire to find a job in which I can combine interaction design and programming. I also love dogs. 🐕
+                </p>
                 <h2>This is me 👇</h2>
                 <img src={me} alt="Picture of me" className="portrait-picture"/>
             </div>
