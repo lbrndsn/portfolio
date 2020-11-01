cbefc3174a15bb3c202eec185be3953bcb61386f
Loïs Berndsen <me@loisberndsen.nl>
2020-11-01 22:26:00 +0100

    all images added

--- src/components/ProjectFlitsmeister2.js
+++ src/components/ProjectFlitsmeister2.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import someImage from '../img/me.jpg';
+import someImage from '../img/cultural-probes.jpg';
+import anotherImage from '../img/persona-michel.png';
 
 const ProjectFlitsmeister2 = () => {
     return (
@@ -11,6 +12,8 @@ const ProjectFlitsmeister2 = () => {
                 This time I didn’t focus on the app, but on the feeling of safety in general. It’s really interesting how different the results were. This project resulted in proposals for complete new additions to the app, which support users in times where they do not feel safe outside (i.e. in the dark), and/of help users avoid situations in which they might feel unsafe.
             </p>
             <img src={someImage}/>
+            <p></p>
+            <img src={anotherImage}/>
             <h3>August 2020</h3>
         </div>
     )
