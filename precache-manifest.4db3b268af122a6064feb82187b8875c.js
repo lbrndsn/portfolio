4304b96a1fdbf4771e465f88a65f0d793fcc706a
Loïs Berndsen <me@loisberndsen.nl>
2021-02-03 20:11:09 +0100

    build

---
+++ precache-manifest.4db3b268af122a6064feb82187b8875c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc96a3e98b9ef74deacd1def39c23fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9dd31d1d92f8c95973e",
+    "url": "/static/css/main.18d49a4d.chunk.css"
+  },
+  {
+    "revision": "cc2226f488f4915f1a76",
+    "url": "/static/js/2.8ceb049b.chunk.js"
+  },
+  {
+    "revision": "f9dd31d1d92f8c95973e",
+    "url": "/static/js/main.82da3ffc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3efc9d525adace42b72221d0b6a772dd",
+    "url": "/static/media/arrow-down.3efc9d52.svg"
+  },
+  {
+    "revision": "5d413dc0f0fb4633ecac1954910604df",
+    "url": "/static/media/cultural-probes.5d413dc0.png"
+  },
+  {
+    "revision": "355f102db5badb93c5744e5cd3ddad0c",
+    "url": "/static/media/dashboard-1 sml.355f102d.png"
+  },
+  {
+    "revision": "0758f5c4bfe7f6b82c7c0c19bcf78d48",
+    "url": "/static/media/empathy-map.0758f5c4.jpg"
+  },
+  {
+    "revision": "25bddb4d9368a11b44bbe4018c282afb",
+    "url": "/static/media/envelope-solid.25bddb4d.svg"
+  },
+  {
+    "revision": "a2a8dfa53177aa2aa4403417753001b3",
+    "url": "/static/media/friend-or-foe.a2a8dfa5.png"
+  },
+  {
+    "revision": "4e4aaacfb3359ce8d4d768bd9c296ea9",
+    "url": "/static/media/ica-presents.4e4aaacf.jpg"
+  },
+  {
+    "revision": "07e868d03c87ea2d2cd420cbcc9ee389",
+    "url": "/static/media/infographic-robin.07e868d0.png"
+  },
+  {
+    "revision": "3720f0d2fd1e97b5ca7baa8224bd4036",
+    "url": "/static/media/linked-in.3720f0d2.svg"
+  },
+  {
+    "revision": "b78fcc652355cb5c546047d76f84e9fb",
+    "url": "/static/media/me.b78fcc65.jpg"
+  },
+  {
+    "revision": "d2dc28ce1d8fbe9ac7f75ebb189de3a9",
+    "url": "/static/media/mockup-dashboard-workload-robin sml.d2dc28ce.png"
+  },
+  {
+    "revision": "6e994bc4e700fcc796d4c035864fe017",
+    "url": "/static/media/persona-michel.6e994bc4.png"
+  },
+  {
+    "revision": "057a219e0d1866696db73e3b95dab283",
+    "url": "/static/media/spacex2.057a219e.jpg"
+  },
+  {
+    "revision": "75c6d2f2aa0ba2b1b4d0b73b95e5f437",
+    "url": "/static/media/the art of waiting.75c6d2f2.png"
+  },
+  {
+    "revision": "06251b78ad96e22a8a8d99672a0f33de",
+    "url": "/static/media/weightables.06251b78.png"
+  },
+  {
+    "revision": "c61eea0e261783cca6e696e4247c1d5a",
+    "url": "/static/media/wintersport-rev-robin.c61eea0e.png"
+  }
+]);
